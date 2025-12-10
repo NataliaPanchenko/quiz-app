28544e6952a374c73b80b966fd64977eff240c62
Natalia <nataliapanchenko@ukr.net>
2025-12-10 09:48:34 +0100

    edit card borders and quiz name

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.querySelectorAll(".card").forEach((card) => {
   card.querySelector(".show__answer").onclick = () => {
-    card.querySelector(".card__answer").classList.toggle("hidden");
+    card.querySelector(".card__answer").classList.toggle("active");
   };
 });
